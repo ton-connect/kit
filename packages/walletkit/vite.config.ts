d2361bfcaf26e1823069194795f78f56648225d9
Tim <truecarry@gmail.com>
2025-08-25 11:39:05 +0300

    Add vite for bundle analyzer

---
+++ packages/walletkit/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { analyzer } from 'vite-bundle-analyzer';
+
+export default defineConfig({
+    plugins: [analyzer()],
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'WalletKit',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
+        },
+        rollupOptions: {
+            external: ['buffer'],
+        },
+    },
+    // define: {
+    //     global: 'globalThis',
+    // },
+    // optimizeDeps: {
+    //     include: ['buffer'],
+    // },
+});
