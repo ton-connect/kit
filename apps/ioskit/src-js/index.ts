28115a43054bbc078262356d471276e3b879136f
Tim <truecarry@gmail.com>
2025-09-09 00:20:17 +0300

    Polyfill fetch to self

--- apps/ioskit/src-js/index.ts
+++ apps/ioskit/src-js/index.ts
@@ -4,9 +4,22 @@ if (typeof window !== 'undefined') {
 }
 if (typeof globalThis !== 'undefined') {
     textEncoder(globalThis);
+    if (typeof window !== 'undefined') {
+        self.fetch = window.fetch;
+    }
 }
 if (typeof global !== 'undefined') {
     textEncoder(global);
+    if (typeof window !== 'undefined') {
+        self.fetch = window.fetch;
+    }
+}
+
+if (typeof self !== 'undefined') {
+    textEncoder(self);
+    if (typeof window !== 'undefined') {
+        self.fetch = window.fetch;
+    }
 }
 
 import('./firstPolyfill');
