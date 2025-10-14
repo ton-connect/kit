20e68121e97b13684446c31992dc9ffeb88da1d9
Nikita Rodionov <proud9proud@gmail.com>
2025-10-14 15:04:48 +0300

    Clean up

--- apps/TONWalletApp/Packages/TONWalletKit/Sources/TONWalletKit/JSEngine/Polyfilling/Fetch/Core/JSValue+PrivateSymbol.swift
+++
@@ -1,7 +0,0 @@
-import JavaScriptCore
-
-extension JSValue {
-  convenience init(privateSymbolIn context: JSContext) {
-    self.init(newSymbolFromDescription: "_jsCoreExtrasPrivate", in: context)
-  }
-}
--- apps/TONWalletApp/Packages/TONWalletKit/Sources/TONWalletKit/JSEngine/Polyfilling/Fetch/Core/JSValue+Utils.swift
+++
@@ -1,7 +0,0 @@
-import JavaScriptCore
-
-extension JSValue {
-  public var isIterable: Bool {
-    self.hasProperty(self.context.evaluateScript("Symbol.iterator"))
-  }
-}
