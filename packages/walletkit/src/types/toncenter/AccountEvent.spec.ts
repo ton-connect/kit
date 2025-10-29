7b23c7b24959dc06e3807db70b883fcf981edcb6
Ilyar <ilyar.software@gmail.com>
2025-10-29 10:07:26 +0100

    chore: add test data for event ft sent

--- packages/walletkit/src/types/toncenter/AccountEvent.spec.ts
+++ packages/walletkit/src/types/toncenter/AccountEvent.spec.ts
@@ -7,6 +7,7 @@ const account = 'EQCdqXGvONLwOr3zCNX5FjapflorB6ZsOdcdfLrjsDLt3Fy9';
 const addressBook: AddressBook = {
     EQCdqXGvONLwOr3zCNX5FjapflorB6ZsOdcdfLrjsDLt3Fy9: 'tolya.ton',
     'EQCLcEJJ4Bj6pZvcNW9GOno0_AIB27-nRBo4e-g-n_-_xDjd': 'pumpanddump.ton',
+    EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs: 'usdt-minter.ton',
 };
 
 describe('AccountEvent', () => {
@@ -207,6 +208,75 @@ describe('AccountEvent', () => {
                     ],
                 },
                 baseTransactions: ['0xdd3fd249b31515b60d4ff242820e7eda0ceca49d7a42a2c874ac31dcfb43cb9c'],
+            },
+        ]);
+    });
+
+    it('ft sent', async () => {
+        const traces = loadData<ToncenterTracesResponse>('ft-sent-traces');
+        const actual = toEvent(traces.traces[0], account, addressBook);
+        expect(actual.actions).toEqual([
+            {
+                type: 'JettonTransfer',
+                status: 'success',
+                JettonTransfer: {
+                    sender: {
+                        address: 'EQCdqXGvONLwOr3zCNX5FjapflorB6ZsOdcdfLrjsDLt3Fy9',
+                        name: 'tolya.ton',
+                        isScam: false,
+                        isWallet: true,
+                    },
+                    recipient: {
+                        address: 'EQBONmT67oFPvbbByzbXK6xS0V4YbBHs1mT-Gz8afP2AHYFo',
+                        isScam: false,
+                        isWallet: true,
+                    },
+                    sendersWallet: '0:c4072d7b04ab4c504cd223dfde42c8037c6a99272d3624aec2cbff2e72b102dd',
+                    recipientsWallet: '0:9f27ad7db6c58cbfe24c03be35766209dca0292bb7e45482f8b5912fa11bf300',
+                    amount: 1000000n,
+                    comment: 'Hello!',
+                    jetton: {
+                        address: 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs',
+                        name: 'Tether USD',
+                        symbol: 'USD₮',
+                        decimals: 6,
+                        image: 'https://cache.tonapi.io/imgproxy/T3PB4s7oprNVaJkwqbGg54nexKE0zzKhcrPv8jcWYzU/rs:fill:200:200:1/g:no/aHR0cHM6Ly90ZXRoZXIudG8vaW1hZ2VzL2xvZ29DaXJjbGUucG5n.webp',
+                        verification: 'whitelist',
+                        score: 100,
+                    },
+                },
+                simplePreview: {
+                    name: 'Jetton Transfer',
+                    description: 'Transferring 1 USD₮',
+                    value: '1 USD₮',
+                    valueImage:
+                        'https://cache.tonapi.io/imgproxy/T3PB4s7oprNVaJkwqbGg54nexKE0zzKhcrPv8jcWYzU/rs:fill:200:200:1/g:no/aHR0cHM6Ly90ZXRoZXIudG8vaW1hZ2VzL2xvZ29DaXJjbGUucG5n.webp',
+                    accounts: [
+                        {
+                            address: 'EQBONmT67oFPvbbByzbXK6xS0V4YbBHs1mT-Gz8afP2AHYFo',
+                            isScam: false,
+                            isWallet: true,
+                        },
+                        {
+                            address: 'EQCdqXGvONLwOr3zCNX5FjapflorB6ZsOdcdfLrjsDLt3Fy9',
+                            name: 'tolya.ton',
+                            isScam: false,
+                            isWallet: true,
+                        },
+                        {
+                            address: 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs',
+                            name: 'usdt-minter.ton',
+                            isScam: false,
+                            isWallet: false,
+                        },
+                    ],
+                },
+                base_transactions: [
+                    '0x55a1273fb40df01f189902c8ea0a9f49dbc4b5858a0b3e3d88b67e8bbd8f7fd9',
+                    '0x027f35335ad0de2d64a62d6ee066f7c85b42bb26667c1e1c922c45bf7060aece',
+                    '0xf61b0d813f307936901292b8e7681b9a44d63825e408efca77e54fd56c68f3a9',
+                    '0x33f7326c8a709967f5611a1c88b9f4c52e2ec95fe59b5f09a2bf89162945bb71',
+                ],
             },
         ]);
     });
