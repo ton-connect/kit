7429da9f95c369985321027b446f62d81d839f29
Tim <truecarry@gmail.com>
2026-01-29 14:50:08 +0400

    feat(vite): enable bundle analyzer based on environment variable

--- apps/demo-wallet/vite.config.ts
+++ apps/demo-wallet/vite.config.ts
@@ -11,14 +11,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-// import bundleAnalyzer from 'vite-bundle-analyzer';
+import bundleAnalyzer from 'vite-bundle-analyzer';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        // bundleAnalyzer()
+        process.env.ANALYZE === 'true' ? bundleAnalyzer() : null, // enable analyzer under env
     ],
     server: {
         allowedHosts: ['localhost', '127.0.0.1', 'local.dev'],
